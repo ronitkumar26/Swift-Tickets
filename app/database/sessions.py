fe88806140386cfaed6e992f9c32d4cb75ee244b
ronitkumar09262006-debug <ronitkumar09262006@gmail.com>
2026-02-11 16:15:37 +0530

    feat: setup PostgreSQL database session and base model

---
+++ app/database/sessions.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True  # logs SQL queries (dev only)
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
